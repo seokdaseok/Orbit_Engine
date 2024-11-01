c581f414f10f12aded285199279158533c73ce0a
Swok <64110712+seokdaseok@users.noreply.github.com>
2024-11-01 21:31:34 +0800

    Update run.py

--- Orbit Engine/run.py
+++ Orbit Engine/run.py
@@ -5,27 +5,33 @@ import orbitCalcBasic as OCB
 
 import velocityVerlet as vV
 
+import ODECalc as ODE
+
 import initial as init
 
-import odeTest as ode2b
 
 _timeStep = 0.001
-duration = 2.0
+duration = 6.34
 
-t_eval_step = 10000
+t_eval_step = round(duration / _timeStep)
 
 run_time_taken = 0
+
+_rtol = 1e-9
 
 planet_list_data = np.array([])
 
 planet_position_data = np.array([])
 
-def set_conditions(ts, dur):
+def set_conditions(ts, dur, rtol):
     global _timeStep
     _timeStep = ts
 
     global duration
     duration = dur
+
+    global _rtol
+    _rtol = rtol
 
 
 def run_thing():
@@ -34,9 +40,10 @@ def run_thing():
     if(len(planetsFinalList) >= 1):
 
         #velocity verlet
-        vV._simLoop(_timeStep, duration, planetsFinalList)
-        global run_time_taken
-        run_time_taken = vV.run_time_taken
+
+        # vV._simLoop(_timeStep, duration, planetsFinalList)
+        # global run_time_taken
+        # run_time_taken = vV.run_time_taken
 
         #OCB
 
@@ -44,22 +51,49 @@ def run_thing():
         # global run_time_taken
         # run_time_taken = OCB.run_time_taken
 
-        ##using ode stuff
-        # init_conditions = [planetsFinalList[0][0:6], planetsFinalList[1][0:6]]
-        # masses = [planetsFinalList[0][6], planetsFinalList[1][6]]
-        # masses = np.array(masses)
+        #ODE
 
-        # print(init_conditions)
-        # print(masses)
+        ode_positions = []
+        ode_velocities = []
+        ode_masses = []
 
-        # t_span = (0, duration)
+        print("List of Planets: ", planetsFinalList, "\n")
 
-        # t_eval = np.linspace(t_span[0], t_span[1], t_eval_step)
+        for planet in planetsFinalList:
+            ode_positions.append(planet[:3])
+            ode_velocities.append(planet[3:6])
+            ode_masses.append(planet[6])
 
-        # ode2b.simulate(init_conditions, masses, t_span, t_eval)
+        ode_positions = np.array(ode_positions)
+        ode_velocities = np.array(ode_velocities)
+        ode_masses = np.array(ode_masses)
+
+        ODE.initial_pos = ode_positions
+        ODE.initial_vel = ode_velocities
+        ODE.masses = ode_masses
+
+        ode_t_span = (0, duration)
+        ode_t_eval = np.linspace(ode_t_span[0], ode_t_span[1], t_eval_step)
+
+        solutions = ODE.simulate_n_body_gaussian(ode_t_span, _rtol, ode_t_eval)
+
+        #print(solutions)
+
+        N = len(ode_masses)
+        print("Solutions Shape: ", solutions.y.shape)
+        print("Solutions: ", solutions.y, "\n")
+
+        global planet_position_data
+        planet_position_data = solutions.y[:3*N].reshape((N, 3, -1))
+
+        print("Position Data: ", planet_position_data, "\n")
+
+        global run_time_taken
+        run_time_taken = ODE.run_time_taken
+
 
 def get_results():
 
     global planet_position_data
-    planet_position_data = vV.position_results
+    #planet_position_data = vV.position_results
     #planet_position_data = ode2b.planet_position_data
